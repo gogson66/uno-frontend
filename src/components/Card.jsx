451617b12a8f85a08f23638069be02ca7063620d
Goran <cvijanovicgoran78@gmail.com>
2026-02-10 21:10:03 +0100

    Add css

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h3>Card</h3>
+      <div >
+        <span>RED 5</span>
+      </div>
+    </div>
+  )
+}
+
+export default FrontCard
--- src/components/PlayerHand.jsx
+++ src/components/PlayerHand.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './PlayerHand.css'
 
 const PlayerHand = () => {
   return (
     <div>
       <h3>Your Cards</h3>
-      <div style={{display: 'flex', gap: '10px'}}>
+      <div>
         <button>Card 1</button>
         <button>Card 2</button>
         <button>Card 3</button>
